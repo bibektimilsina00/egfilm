899cc9b457532bcee85a23342054c2fc1d88aa49
prashant8khaitwada <prashantkhatiwada554@gmail.com>
2025-10-22 14:07:53 +0545

    small fixes

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -220,7 +220,7 @@ export default function MoviesPage() {
                         {/* Movies Grid */}
                         {movies.length > 0 ? (
                             <>
-                                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+                                <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
                                     {/*eslint-disable-next-line @typescript-eslint/no-explicit-any */}
                                     {movies.map((movie: any) => (
                                         <MediaCard
